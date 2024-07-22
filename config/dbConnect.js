28f3b744c59f0824470454924a0ebe3d1f347fa0
Sushank Yadav <119575978+Sushankyadav11@users.noreply.github.com>
2024-07-22 14:51:40 +0530

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         // console.log('connecting to the database')
 
         //connect to the database
-        await mongoose.connect('mongodb://localhost:27017/');
+        await mongoose.connect('mongodb+srv://user:123@sushank.qsoeioy.mongodb.net/sushank?retryWrites=true&w=majority&appName=Sushank');
     }catch(err){
         console.log('Error while connecting database: ', err);
     }
